4714d89a4fb0f2b313e4c443c0be663de2f5c454
rob8479 <psyrcs@nottingham.ac.uk>
2018-04-05 18:20:06 +0100

    Implemented Prim's Algorithm
    Prim's Algorithm has been implemented.
    
    +Updated Grid to store location and parents
    +Added functions which make priority queue working (NOTE: Optimisation can be done here)
    +Main Prim's Loop for Maze Generation added

--- MazeGeneration/WebContent/assets/canvas/Level.js
+++ MazeGeneration/WebContent/assets/canvas/Level.js
@@ -33,8 +33,10 @@ Level.prototype.create = function () {
  * 
  * The main function that generates a level.
  */
+
 function createMaze(numberOfSections, numberOfLayers){
-	console.log(generateMazeGrid(numberOfSections,numberOfLayers));
+	var temp = generateMazeGrid(numberOfSections,numberOfLayers);
+	runPrims(temp, numberOfSections, numberOfLayers);
 }
 
 /**
@@ -51,18 +53,103 @@ function generateMazeGrid(numberOfSections, numberOfLayers){
 		grid[i] = new Array(numberOfLayers);
 	}
 
+	//Look through and fill the grid
 	for(var i = 0; i < numberOfSections; i++){
 		for(var j = 0; j < numberOfLayers; j++){
-			grid[i][j] = Math.floor((Math.random() * 100) + 1);
-			//grid[i][j] = 0;
+			var data = {}
+			//The weight is a random weight used for maze generation
+			data.weight = Math.floor((Math.random() * 100) + 1);
+			data.x = i;
+			data.y = j;
+			//Parent is the value in the path as to where we came from. This means that this is part of the path, between Parent and this one. Do not draw a line
+			data.parent = null;
+			grid[i][j] = data
 		}
 	}
 	
 	return grid;
 }
 
-function runPrims(){
+function runPrims(maze,numberOfSections, numberOfLayers){
+	// For now, just do a simple array, but this can be implemented more efficently for a PQ
 
+	var frontier = []
+	//Starts from Point 0,0
+	frontier.push(maze[0][0]);
+	maze[0][0].opened = true;
+
+	//Get minimum of the frontier
+	function getMinimum(){
+		var currentMinimumIndex = -1;
+		var currentBestWeight = 2000;
+		//Loop through and find the minimum weight
+		for(var i = 0; i < frontier.length; i++){
+			if(frontier[i].weight < currentBestWeight){
+				currentMinimumIndex = i;
+			}
+		}
+		//Error Check
+		if(currentMinimumIndex == -1){
+			console.log("Wuh woah: Error line 92 - Frontier is empty, yet was still called");
+		} else {
+			//Return the minimum
+			return frontier.splice(currentMinimumIndex,1)[0];
+		}
+	}
+
+	//Get neighbours
+	function getNeighbours(cell){
+		//As long as it is bounds
+		//if parent is null, updated parent, then add to list
+		//Null would mean that we have not already gotten here from some other root
+		
+				/**
+		 * 		|y-1|
+		 * 	 x-1| O	|x+1
+		 * 		|y+1|
+		 */
+		
+		// Left
+		if(cell.x != 0){
+			if(maze[cell.x - 1][cell.y].parent == null){
+				maze[cell.x - 1][cell.y].parent = cell;
+				frontier.push(maze[cell.x - 1][cell.y]);
+			}	
+		}
+
+		//Right
+		if(cell.x != numberOfSections - 1){
+			if(maze[cell.x + 1][cell.y].parent == null){
+				maze[cell.x + 1][cell.y].parent = cell;
+				frontier.push(maze[cell.x + 1][cell.y]);
+			}	
+		}
+
+		//Up
+		if(cell.y != 0){
+			if(maze[cell.x][cell.y - 1].parent == null){
+				maze[cell.x][cell.y - 1].parent = cell;
+				frontier.push(maze[cell.x][cell.y - 1]);
+			}
+		}
+
+		//Down
+		if(cell.y != numberOfLayers - 1){
+			if(maze[cell.x][cell.y + 1].parent == null){
+				maze[cell.x][cell.y + 1].parent = cell;
+				frontier.push(maze[cell.x][cell.y + 1]);
+			}
+		}
+
+
+	}
+
+	//Whilst we still have elements in the frontier, get the cell with the minimum weight in the frontier, and then get its neighbours
+	while(frontier.length > 0){
+		var min = getMinimum();
+		getNeighbours(min);
+	}
+	
 }
 
 function drawMaze(){
