0f840ed8a324af2ea71164aad5cd5eecd2716470
rob8479 <psyrcs@nottingham.ac.uk>
2018-04-05 10:56:53 +0100

    Generate Maze Grid
    Created Function that creates a 2D array of noise. This will be used to run the maze generation algorithm on.

--- MazeGeneration/WebContent/assets/canvas/Level.js
+++ MazeGeneration/WebContent/assets/canvas/Level.js
@@ -2,9 +2,9 @@
  * Level.
  */
 function Level() {
-
+	
 	Phaser.State.call(this);
-
+	
 }
 
 /** @type Phaser.State */
@@ -12,27 +12,64 @@ var Level_proto = Object.create(Phaser.State.prototype);
 Level.prototype = Level_proto;
 Level.prototype.constructor = Level;
 
-Level.prototype.init = function() {
-
+Level.prototype.init = function () {
+	
 	this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
 	this.scale.pageAlignHorizontally = true;
 	this.scale.pageAlignVertically = true;
 	this.stage.backgroundColor = '#ffffff';
-
+	
 };
 
-Level.prototype.preload = function() {
+Level.prototype.create = function () {
 
+	createMaze(10,4);
 };
 
-Level.prototype.create = function() {
-	// to change this code: Canvas editor > Configuration > Editor > userCode > Create
-	this.addHelloWorldText();
+/**
+ * 
+ * @param {*} numberOfSections - The number of "spokes". These are the number of grid cells we have per layer  
+ * @param {*} numberOfLayers - The number of layers the total maze has. I.e. - the number of nested circles
+ * 
+ * The main function that generates a level.
+ */
+function createMaze(numberOfSections, numberOfLayers){
+	console.log(generateMazeGrid(numberOfSections,numberOfLayers));
+}
 
-};
+/**
+ * 
+ * @param {*} numberOfSections - The width of the grid
+ * @param {*} numberOfLayers - the height of the grid
+ * 
+ * @returns a numberOfSections x numberOfLayers grid, which each value being random noise.
+ */
+function generateMazeGrid(numberOfSections, numberOfLayers){
+	//Create a 2D array... oh Javascript :l 
+	var grid = new Array(numberOfSections);
+	for (var i = 0; i < numberOfSections; i++) {
+		grid[i] = new Array(numberOfLayers);
+	}
 
-/* --- end generated code --- */
+	for(var i = 0; i < numberOfSections; i++){
+		for(var j = 0; j < numberOfLayers; j++){
+			grid[i][j] = Math.floor((Math.random() * 100) + 1);
+			//grid[i][j] = 0;
+		}
+	}
+	
+	return grid;
+}
 
-Level.prototype.addHelloWorldText = function() {
-	this.add.text(100, 100, "hello world!");
-};
+function runPrims(){
+
+}
+
+function drawMaze(){
+
+}
+
+function addStars(){
+
+}
+
